d9c8b763fe5aaf444098d10a1047570f8563b37f
Abhinay Reddy <abhinayreddy0112@gmail.com>
2025-08-25 19:09:29 +0530

    Initial Commit

---
+++ JobsforceOverlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JobsforceOverlay
+//
+//  Created by Abhinay Prakash Reddy on 8/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JobsforceOverlay/JobsforceOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobsforceOverlayApp.swift
+//  JobsforceOverlay
+//
+//  Created by Abhinay Prakash Reddy on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct JobsforceOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
